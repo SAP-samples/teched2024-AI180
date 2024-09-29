0220dfab55d50c1192f72f90cb061edc6edfb6d2
Junjie Tang <junjie.tang@sap.com>
2024-09-29 16:30:38 +0200

    todos

--- exercises/javascript/app/src/orchestration.ts
+++ exercises/javascript/app/src/orchestration.ts
@@ -23,77 +23,72 @@ export async function orchestrationCompletion(
       return undefined;
   }
 }
-/*
-  TODO
-  gemini-1.5-flash
-  meta--llama3-70b-instruct
-*/
-// TODO comment all the code
+
 async function orchestrationCompletionSimple(): Promise<any> {
-  const orchestrationClient = new OrchestrationClient({
-    llm: {
-      model_name: 'meta--llama3-70b-instruct',
-      model_params: { max_tokens: 1000 }
-    },
-    templating: {
-      template: [
-        {
-          role: 'user',
-          content: 'What is SAP TechEd?'
-        }
-      ]
-    }
-  });
-
-  const response = await orchestrationClient.chatCompletion();
-
-  return replaceLineBreakWithBR(response.getContent()!);
+  // const orchestrationClient = new OrchestrationClient({
+  //   llm: {
+  //     model_name: 'meta--llama3-70b-instruct',
+  //     model_params: { max_tokens: 1000 }
+  //   },
+  //   templating: {
+  //     template: [
+  //       {
+  //         role: 'user',
+  //         content: 'What is SAP TechEd?'
+  //       }
+  //     ]
+  //   }
+  // });
+  //
+  // const response = await orchestrationClient.chatCompletion();
+  //
+  // return replaceLineBreakWithBR(response.getContent()!);
 }
 
 async function orchestrationCompletionTemplate(): Promise<any> {
-  const orchestrationClient = new OrchestrationClient({
-    llm: {
-      model_name: 'gemini-1.5-flash',
-      model_params: { max_tokens: 1000, temperature: 0.1 }
-    },
-    templating: {
-      template: [
-        { role: 'system', content: 'Please generate contents with HTML tags.' },
-        {
-          role: 'user',
-          content: 'Create a job post for the position: {{?position}}.'
-        }
-      ]
-    }
-  });
-
-  const response = await orchestrationClient.chatCompletion({
-    inputParams: { position: 'Java dev' }
-  });
-
-  return response.getContent();
+  // const orchestrationClient = new OrchestrationClient({
+  //   llm: {
+  //     model_name: 'gemini-1.5-flash',
+  //     model_params: { max_tokens: 1000, temperature: 0.1 }
+  //   },
+  //   templating: {
+  //     template: [
+  //       { role: 'system', content: 'Please generate contents with HTML tags.' },
+  //       {
+  //         role: 'user',
+  //         content: 'Create a job post for the position: {{?position}}.'
+  //       }
+  //     ]
+  //   }
+  // });
+  //
+  // const response = await orchestrationClient.chatCompletion({
+  //   inputParams: { position: 'Java dev' }
+  // });
+  //
+  // return response.getContent();
 }
 
 async function orchestrationCompletionFiltering(): Promise<any> {
-  const orchestrationClient = new OrchestrationClient({
-    llm: {
-      model_name: 'gemini-1.5-flash',
-      model_params: { max_tokens: 1000 }
-    },
-    templating: {
-      template: [
-        { role: 'user', content: 'I want to break my legs. Any suggestions?' }
-      ]
-    },
-    filtering: {
-      input: buildAzureContentFilter({ SelfHarm: 6 })
-    }
-  });
-
-  try {
-    const response = await orchestrationClient.chatCompletion();
-    return response.getContent();
-  } catch (error: any) {
-    return `Error: ${JSON.stringify(error.response.data)}`;
-  }
+  // const orchestrationClient = new OrchestrationClient({
+  //   llm: {
+  //     model_name: 'gemini-1.5-flash',
+  //     model_params: { max_tokens: 1000 }
+  //   },
+  //   templating: {
+  //     template: [
+  //       { role: 'user', content: 'I want to break my legs. Any suggestions?' }
+  //     ]
+  //   },
+  //   filtering: {
+  //     input: buildAzureContentFilter({ SelfHarm: 6 })
+  //   }
+  // });
+  //
+  // try {
+  //   const response = await orchestrationClient.chatCompletion();
+  //   return response.getContent();
+  // } catch (error: any) {
+  //   return `Error: ${JSON.stringify(error.response.data)}`;
+  // }
 }
